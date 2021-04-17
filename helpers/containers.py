65b0600afcb5245bf8a688a6ea174fd2863602c3
adrixop95 <adrixop95@me.com>
2021-04-17 17:53:21 +0200

    fix code checks

--- helpers/containers.py
+++ helpers/containers.py
@@ -18,12 +18,16 @@ def get_host_from_traefik():
 
         # if container have host definition in labels
         for key, value in container_from_id.items():
-            if value.startswith('Host'):
-                # regex stuff to filter hostname
-                host_find_brackets = re.findall(r"\(.*?\)", value)[0]
-                host_remove_quotes = re.sub("[(')]", '', host_find_brackets)[1:-1]
-                # lazy check, because I don't want to check the current host with fastapi
-                if "nginx" in host_remove_quotes:
-                    container_hosts.append(host_remove_quotes)
+            try:
+                if value.startswith('Host'):
+                    # regex stuff to filter hostname
+                    host_find_brackets = re.findall(r"\(.*?\)", value)[0]
+                    host_remove_quotes = re.sub("[(')]", '', host_find_brackets)[1:-1]
+                    # lazy check, because I don't want to check the current host with fastapi
+                    if "nginx" in host_remove_quotes:
+                        container_hosts.append(host_remove_quotes)
+            except TypeError:
+                message = "This " + value + key + "is incorrect!"
+                container_hosts.append(message)
 
     return container_hosts
