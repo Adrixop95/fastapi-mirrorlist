4b797be0290242cc4068158b57e3cd25cd37b85e
adrixop95 <adrixop95@me.com>
2021-04-17 21:12:17 +0200

    add env variables

--- app/main.py
+++ app/main.py
@@ -1,28 +1,33 @@
+import ast
+import os
+
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
+from dotenv import load_dotenv
+
 from helpers.containers import get_host_from_traefik
 
 app = FastAPI()
+load_dotenv(verbose=True)
+
+traefik_hosts = os.environ.get('traefik_hosts')
+provided_hosts = os.environ.get('provided_hosts')
 
 
 @app.get("/", response_class=PlainTextResponse)
 async def root(release: str = "", repo: str = "", arch: str = "x86_64"):
-
     final_url = ""
+    url_list = list(provided_hosts.split(","))
+
+    if ast.literal_eval(traefik_hosts):
+        url_list.extend(get_host_from_traefik())
 
     if repo and release:
-
-        for traefik_url in get_host_from_traefik():
+        for mirror_url in url_list:
             # not the best way to generate url, ikr
-            nginx_url = "http://" + traefik_url + "/" + release + "/" + repo + "/" + arch + "/os/"
+            nginx_url = "http://" + mirror_url + "/" + release + "/" + repo + "/" + arch + "/os/"
 
-            # Here you should check if a given URl exists, then add it to the list, eg code:
-            # import requests
-            # nginx_request = requests.get(nginx_url, stream=True, verify=False)
-            #     if nginx_request.status_code == 200:
-            #         final_url.append(nginx_url)
-
-            final_url = final_url+nginx_url+"\n"
+            final_url = final_url + nginx_url + "\n"
         return final_url
 
     else:
