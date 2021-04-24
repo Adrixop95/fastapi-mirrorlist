1dae5e809550452e63d701e9c05a8a538236da5e
adrixop95 <adrixop95@me.com>
2021-04-24 12:39:04 +0200

    fix flake8

--- helpers/traefik_labels.py
+++ helpers/traefik_labels.py
@@ -1,17 +1,19 @@
 import re
 import docker
 
+
 def add_to_list(list, string):
     string += '{0}'
     list = [string.format(i) for i in list]
     return list
+
 
 def get_host_from_traefik():
     client_api = docker.APIClient(base_url='unix://var/run/docker.sock')
     client_env = docker.from_env()
 
     running_containers = client_env.containers.list(all=True)
-    
+
     container_hosts = []
 
     # senselessly high computational complexity to be rewritten
@@ -22,15 +24,14 @@ def get_host_from_traefik():
                 if value.count('`') > 4:
                     unified_value = value.replace(" ", "")
                     base_host = unified_value.partition("Host(`")[2].partition("`)")[0]
-                    
-                    prefixes = re.sub (r"[(|)|`|&|\s+]",r"", \
-                        unified_value.partition("&&PathPrefix(`")[2].partition('`)')[0])\
-                            .split(",")
 
-                    container_hosts.extend(add_to_list(prefixes,base_host))
+                    prefixes = re.sub(r"[(|)|`|&|\s+]", r"",
+                                      unified_value.partition("&&PathPrefix(`")[2].partition('`)')[0]).split(",")
+
+                    container_hosts.extend(add_to_list(prefixes, base_host))
                 else:
-                    regex_cleanup = re.sub(r"[(|)|`|&|\s+]",r"",value)
-                    string_cleanup = regex_cleanup.replace("Host","").replace("PathPrefix","").strip()
+                    regex_cleanup = re.sub(r"[(|)|`|&|\s+]", r"", value)
+                    string_cleanup = regex_cleanup.replace("Host", "").replace("PathPrefix", "").strip()
                     container_hosts.append(string_cleanup)
-    
+
     return container_hosts
